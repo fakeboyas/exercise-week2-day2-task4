3a74abd187bead0509a942557e4e2b5de7f0d0e0
fakeboyas <ovoivi9@gmail.com>
2020-06-16 15:51:16 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function ganti() {
+    let element = document.getElementById("hello");
+    element.classList.add("hello");
+    let hapus = document.getElementById("world");
+    hapus.remove();
+}
